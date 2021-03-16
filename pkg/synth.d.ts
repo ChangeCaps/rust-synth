daab74be5e12d6af3bf4fcc4347fb5c836f35a70
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2021-03-16 14:52:23 +0100

    added pkg

---
+++ pkg/synth.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} canvas_id
+*/
+export function start(canvas_id: string): void;
---
+++ pkg/synth.js
@@ -0,0 +1,2 @@
+import * as wasm from "./synth_bg.wasm";
+export * from "./synth_bg.js";
